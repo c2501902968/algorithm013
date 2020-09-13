42e56ff6fa8da261c11f9fd5ac2e96706cfc26ea
chengzi <chengjie2z@163.com>
2020-09-13 21:31:31 +0800

    第7周作业，好难～

---
+++ Week_07/trie.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+class Trie(object):
+    
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+        self.end_of_word = "#"
+
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: None
+        """
+        node = self.root
+        for char in word:
+            node = node.setdefault(char,{})
+        node[self.end_of_word] = self.end_of_word
+
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        return self.end_of_word in word
+
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for char in prefix:
+            if char not in node:
+                return False
+            node = node[char]
+        return True
+
+if __name__ == "__main__":
+    Trie trie = new Trie();
+    trie.insert("apple")
+    trie.search("apple")
+    trie.search("app")
+    trie.startsWith("app")
+    trie.insert("app")
+    trie.search("app")
+    
